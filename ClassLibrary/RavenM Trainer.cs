694c598dfaee018ae05bc4c2744b66d307a5f94a
fn020765 <>
2025-06-14 23:14:18 +0100

    删除旧的 .gitignore 文件和项目文件，添加新的项目文件和插件代码

---
+++ ClassLibrary/RavenM Trainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace RavenfieldTrainer
+{
+    [BepInPlugin("com.yourname.ravenfield.trainer", "Ravenfield Trainer", "0.1.0")]
+    public class RavenfieldTrainer : BaseUnityPlugin
+    {
+        private Harmony _harmony;
+
+        private void Awake()
+        {
+            _harmony = new Harmony("com.yourname.ravenfield.trainer");
+            _harmony.PatchAll();
+
+            Logger.LogInfo("Ravenfield Trainer loaded, Harmony patches applied.");
+        }
+
+        private void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+            Logger.LogInfo("Ravenfield Trainer unloaded, Harmony patches removed.");
+        }
+    }
+}
+
