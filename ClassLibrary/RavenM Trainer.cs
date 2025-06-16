b0f92a05054a0e71d7bd08c8d7fdc1568ee47c50
fn020765 <>
2025-06-16 04:39:38 +0100

    oopsies

--- ClassLibrary/RavenM Trainer.cs
+++ ClassLibrary/RavenM Trainer.cs
@@ -1,30 +1,73 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using HarmonyLib;
 using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
+using Ravenfield.SpecOps;
+using Ravenfield.Trigger;
 
-
-namespace RavenfieldTrainer
+namespace RavenM.Trainer
 {
-    [BepInPlugin("com.yourname.ravenfield.trainer", "Ravenfield Trainer", "0.1.0")]
-    public class RavenfieldTrainer : BaseUnityPlugin
+    [BepInPlugin("com.example.ravenmtrainer", "RavenM Trainer", "0.1.0")]
+    public class TrainerPlugin : BaseUnityPlugin
     {
-        private Harmony _harmony;
+        // Logger instance (hide base Logger)
+        private static new ManualLogSource Logger;
+        
+        // Toggle for GUI display
+        private bool _showGui = false;
 
         private void Awake()
         {
-            _harmony = new Harmony("com.yourname.ravenfield.trainer");
-            _harmony.PatchAll();
-
-            Logger.LogInfo("Ravenfield Trainer loaded, Harmony patches applied.");
+            // Initialize logger
+            Logger = base.Logger;
+            Logger.LogInfo("RavenM Trainer loaded");
+            
+            // Apply Harmony patches
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.example.ravenmtrainer");
         }
 
-        private void OnDestroy()
+        private void Update()
         {
-            _harmony.UnpatchSelf();
-            Logger.LogInfo("Ravenfield Trainer unloaded, Harmony patches removed.");
+            // Toggle GUI with F6
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                _showGui = !_showGui;
+                // Update cursor lock state
+                Cursor.visible = _showGui;
+                Cursor.lockState = _showGui ? CursorLockMode.None : CursorLockMode.Locked;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (!_showGui) return;
+
+            UnityEngine.GUILayout.BeginArea(new Rect(10, 10, 300, Screen.height - 20));
+            UnityEngine.GUILayout.Label("=== RavenM Trainer ===");
+
+            if (UnityEngine.GUILayout.Button("Toggle Infinite Ammo"))
+            {
+                // TODO: implement infinite ammo toggle
+                Logger.LogInfo("Infinite Ammo toggled");
+            }
+
+            if (UnityEngine.GUILayout.Button("Toggle God Mode"))
+            {
+                // TODO: implement god mode toggle
+                Logger.LogInfo("God Mode toggled");
+            }
+
+            UnityEngine.GUILayout.EndArea();
         }
     }
 }
-
